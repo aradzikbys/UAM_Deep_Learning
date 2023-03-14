aba7bf812c43fae37c1e0880e1f8177d87e1ff95
agataradzikowska <aradzikowskabystron@gmail.com>
2023-03-15 00:12:26 +0100

    Deep Learning and AutoML project update

--- 01_shrooms.R
+++ 01_shrooms.R
@@ -4,30 +4,25 @@
 library(h2o)
 library(ggplot2)
 library(dplyr)
+library(knitr)
 library(wesanderson)
+
 
 h2o.init(nthreads = -1,
          max_mem_size = '4G')
 
-mushrooms <- read.csv('https://raw.githubusercontent.com/aradzikbys/UAM_Deep_Learning/master/mushroom.csv')
+mushrooms <- read.csv('https://tinyurl.com/hmkhs9au')
 
 # Adapt data set for further analysis
-
 mushrooms <- mushrooms %>%
   # Remove not needed characters
   mutate(across(1:23, ~ substr(.x, 3,3))) %>%
   # Change columns to factor (initially set as strings)
   mutate(across(everything(), as.factor))
 
-skimr::skim(mushrooms)
+head(mushrooms)
 
 mushrooms_hex <- as.h2o(mushrooms, destination_frame = 'mushrooms_hex')
-
-ggplot(mushrooms, aes(cap.shape)) +
-  geom_bar(position="dodge", aes(fill = class)) +
-  scale_fill_manual(name = 'Edible/Poisonous', values = wes_palette('Moonrise2', type = 'discrete')) +
-  labs(x = 'Cap shape', y = 'Count')
-  
 
 # Split data set to train & test
 mushrooms_split <- h2o.splitFrame(data = mushrooms_hex, ratios = 0.75)
@@ -45,14 +40,12 @@ mushrooms_model <- h2o.deeplearning(
                           hidden = c(300, 300, 300),
                           input_dropout_ratio = 0.2,
                           l1 = 1e-5,
-                          epochs = 60,
+                          epochs = 40,
                           variable_importances = TRUE)
-
-mushrooms_model
 
 # Mushrooms predictions
 mushroom_pred <- h2o.predict(object = mushrooms_model,
-                            newdata = mushrooms_test)
+                             newdata = mushrooms_test)
 
 # Confusion matrix from h2o functions
 h2o.performance(mushrooms_model, valid = TRUE)
@@ -61,5 +54,43 @@ h2o.performance(mushrooms_model, valid = TRUE)
 table(as.data.frame(mushrooms_test[,23])[,1],
       as.data.frame(mushroom_pred[,1])[,1])
 
+
+# Model evaluation
+expl_mushroom <- h2o.explain(mushrooms_model, mushrooms_test)
+print(expl_mushroom)
+
+# Variables importance
+h2o.varimp_plot(mushrooms_model)
+
+# Odor vs class
+ggplot(mushrooms, aes(odor)) +
+  geom_bar(position="dodge", aes(fill = class)) +
+  scale_fill_manual(name = 'Edible/Poisonous', values = wes_palette('Moonrise2', type = 'discrete')) +
+  labs(x = 'Odor', y = 'Count')+
+  scale_x_discrete(labels=c('almond', 'creosote', 'foul', 'anise', 'musty', 'none',
+                            'pungent', 'spicy', 'fishy'))
+
+
+# Odor vs class
+ggplot(mushrooms, aes(spore.print.color)) +
+  geom_bar(position="dodge", aes(fill = class)) +
+  scale_fill_manual(name = 'Edible/Poisonous', values = wes_palette('Moonrise2', type = 'discrete')) +
+  labs(x = 'Odor', y = 'Count')+
+  scale_x_discrete(labels=c('buff', 'chocolate', 'black', 'brown', 'orange', 'green',
+                            'purple', 'white', 'yellow'))
+
+
+mushrooms_auoml <- h2o.automl(y = 23,
+                              x = 1:22,
+                              training_frame = mushrooms_train,
+                              max_runtime_secs = 30,
+                              max_models = 20)
+
+
+h2o.pd_multi_plot(mushrooms_auoml, mushrooms_test, "odor")
+
+df <- h2o.get_leaderboard(object = mushrooms_auoml, extra_columns = "ALL")
+df <- as.data.frame(df)
+df$model_id <- substr(df$model_id,1,5)
 
 h2o.shutdown(prompt = TRUE)
