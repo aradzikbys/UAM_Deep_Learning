f27073504dbc808e1b5bcf2b8ca5b22f556842ff
agataradzikowska <aradzikowskabystron@gmail.com>
2023-04-01 22:08:37 +0200

    Deep Learning - CNN

--- 03_cat_dog_panda.R
+++ 03_cat_dog_panda.R
@@ -1,7 +1,27 @@
+# Source: https://rpubs.com/Argaadya/image_conv
+
+# Tensorflow
+tensorflow::install_tensorflow()
+
+# Deep learning
+library(keras)
+library(tensorflow)
+
+# Data wrangling
+library(tidyverse)
+
+# Image manipulation
+library(imager)
+
+# Model Evaluation
+library(caret)
+
+
+
 getwd()
 
-folder_class <- list.files("C:/Users/User/OneDrive/Edu/Deep Learning/cat_dog_panda/") 
-folder_path <- paste0("C:/Users/User/OneDrive/Edu/Deep Learning/cat_dog_panda/", folder_class, "/")
+folder_class <- list.files('C:/Users/User/OneDrive/Edu/Deep Learning/c_d_p/') 
+folder_path <- paste0('C:/Users/User/OneDrive/Edu/Deep Learning/c_d_p/', folder_class, '/')
 
 folder_path
 
@@ -13,9 +33,6 @@ file_name <- map(folder_path,
 
 # first 6 file name
 head(file_name)
-
-length(file_name)
-
 
 # Desired height and width of images
 target_size <- c(64, 64)
@@ -32,21 +49,21 @@ train_data_gen <- image_data_generator(rescale = 1/255, # Scaling pixel value
                                        zoom_range = 0.25, # Zoom in or zoom out range
                                        validation_split = 0.2) # 20% data as validation data
 # Training Dataset
-train_image_array_gen <- flow_images_from_directory(directory = "C:/Users/User/OneDrive/Edu/Deep Learning/cat_dog_panda/", # Folder of the data
+train_image_array_gen <- flow_images_from_directory(directory = 'C:/Users/User/OneDrive/Edu/Deep Learning/c_d_p/', # Folder of the data
                                                     target_size = target_size, # target of the image dimension (64 x 64)  
-                                                    color_mode = "rgb", # use RGB color
+                                                    color_mode = 'rgb', # use RGB color
                                                     batch_size = batch_size , 
                                                     seed = 123,  # set random seed
-                                                    subset = "training", # declare that this is for training data
+                                                    subset = 'training', # declare that this is for training data
                                                     generator = train_data_gen)
 
 # Validation Dataset
-val_image_array_gen <- flow_images_from_directory(directory = "C:/Users/User/OneDrive/Edu/Deep Learning/cat_dog_panda/",
+val_image_array_gen <- flow_images_from_directory(directory = 'C:/Users/User/OneDrive/Edu/Deep Learning/c_d_p/',
                                                   target_size = target_size, 
-                                                  color_mode = "rgb", 
+                                                  color_mode = 'rgb', 
                                                   batch_size = batch_size ,
                                                   seed = 123,
-                                                  subset = "validation", # declare that this is the validation data
+                                                  subset = 'validation', # declare that this is the validation data
                                                   generator = train_data_gen)                                       
 
 # Number of training samples
@@ -59,21 +76,18 @@ valid_samples <- val_image_array_gen$n
 output_n <- n_distinct(train_image_array_gen$classes)
 
 # Get the class proportion
-table("\nFrequency" = factor(train_image_array_gen$classes)
+table('\nFrequency' = factor(train_image_array_gen$classes)
 ) %>% 
   prop.table()
 
 
 
-model <- keras_model_sequential(name = "simple_model") %>% 
+model <- keras_model_sequential(name = 'simple_model') %>% 
   
   # Convolution Layer
-  layer_conv_2d(filters = 16,
-                kernel_size = c(3,3),
-                padding = "same",
-                activation = "relu",
-                input_shape = c(target_size, 3) 
-  ) %>% 
+  layer_conv_2d(filters = 16, kernel_size = c(3,3),
+                padding = 'same', activation = 'relu',
+                input_shape = c(target_size, 3)) %>% 
   
   # Max Pooling Layer
   layer_max_pooling_2d(pool_size = c(2,2)) %>% 
@@ -83,40 +97,162 @@ model <- keras_model_sequential(name = "simple_model") %>%
   
   # Dense Layer
   layer_dense(units = 16,
-              activation = "relu") %>% 
+              activation = 'relu') %>% 
   
   # Output Layer
   layer_dense(units = output_n,
-              activation = "softmax",
-              name = "Output")
+              activation = 'softmax',
+              name = 'Output')
 
 model
 
 
 model %>% 
   compile(
-    loss = "categorical_crossentropy",
+    loss = 'categorical_crossentropy',
     optimizer = optimizer_adam(),
-    metrics = "accuracy"
-  )
-
-(as.integer(train_samples / batch_size))
-(as.integer(valid_samples / batch_size))
+    metrics = 'accuracy')
 
 # Fit data into model
 history <- model %>% 
-  fit(
-    # Training data
-    # 2400 images belonging to 3 classes.
-    train_image_array_gen,
-    
-    # 75 stops per epoch
-    steps_per_epoch = as.integer(train_samples / batch_size), 
-    # Training epochs
-    epochs = 30, 
-    
-    # validation data
-    # 600 images belonging to 3 classes
+  fit(train_image_array_gen,
+    steps_per_epoch = as.integer(train_samples / batch_size),
+    epochs = 30,
     validation_data = val_image_array_gen,
-    # 18 validation steps
     validation_steps = as.integer(valid_samples / batch_size))
+
+
+plot(history)
+
+
+val_data <- data.frame(file_name = paste0('C:/Users/User/OneDrive/Edu/Deep Learning/c_d_p/',
+                                          val_image_array_gen$filenames)) %>% 
+  mutate(class = str_extract(file_name, 'cat|dog|panda'))
+
+head(val_data, 10)
+tail(val_data,10)
+
+# Function to convert image to array
+image_prep <- function(x) {
+  arrays <- lapply(x, function(path) {
+    img <- image_load(path, target_size = target_size, 
+                      grayscale = F # Set FALSE if image is RGB
+                      )
+    
+    x <- image_to_array(img)
+    x <- array_reshape(x, c(1, dim(x)))
+    x <- x/255 # rescale image pixel
+    })
+  do.call(abind::abind, c(arrays, list(along = 1)))
+}
+
+test_x <- image_prep(val_data$file_name)
+
+# Check dimension of testing data set
+dim(test_x)
+
+# not valid anymore:
+# pred_test <- predict_classes(model, test_x) 
+
+pred_test <- model %>% predict(test_x) %>% k_argmax()
+
+head(pred_test, 10)
+
+# Convert encoding to label
+decode <- function(x){
+  case_when(x == 0 ~ "cat",
+            x == 1 ~ "dog",
+            x == 2 ~ "panda")
+            }
+
+pred_test <- sapply(pred_test, decode) 
+
+head(pred_test, 10)
+
+confusionMatrix(as.factor(pred_test), 
+                as.factor(val_data$class))
+
+
+
+
+model_big <- keras_model_sequential() %>% 
+  
+  # First convolutional layer
+  layer_conv_2d(filters = 32, kernel_size = c(5,5),
+                padding = "same", activation = "relu",
+                input_shape = c(target_size, 3)) %>% 
+  
+  # Second convolutional layer
+  layer_conv_2d(filters = 32, kernel_size = c(3,3),
+                padding = "same", activation = "relu") %>% 
+  
+  # Max pooling layer
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  
+  # Third convolutional layer
+  layer_conv_2d(filters = 64, kernel_size = c(3,3),
+                padding = "same", activation = "relu") %>% 
+  
+  # Max pooling layer
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  
+  # Fourth convolutional layer
+  layer_conv_2d(filters = 128, kernel_size = c(3,3),
+                padding = "same", activation = "relu") %>% 
+  
+  # Max pooling layer
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  
+  # Fifth convolutional layer
+  layer_conv_2d(filters = 256, kernel_size = c(3,3),
+                padding = "same", activation = "relu") %>% 
+  
+  # Max pooling layer
+  layer_max_pooling_2d(pool_size = c(2,2)) %>% 
+  
+  # Flattening layer
+  layer_flatten() %>% 
+  
+  # Dense layer
+  layer_dense(units = 64, activation = "relu") %>% 
+  
+  # Output layer
+  layer_dense(name = "Output", units = 3, activation = "softmax")
+
+model_big
+
+model_big %>% 
+  compile(
+    loss = "categorical_crossentropy",
+    optimizer = optimizer_adam(),
+    metrics = "accuracy")
+
+model_big %>% fit_generator( train_image_array_gen,
+                             steps_per_epoch = as.integer(train_samples / batch_size),
+                             epochs = 50,
+                             validation_data = val_image_array_gen,
+                             validation_steps = as.integer(valid_samples / batch_size)) -> history
+
+plot(history)
+
+
+# not valid anymore:
+# pred_test <- predict_classes(model_big, test_x) 
+
+pred_test <-  model_big %>% predict(test_x) %>% k_argmax()
+
+head(pred_test, 10)
+
+# Convert encoding to label
+decode <- function(x){
+  case_when(x == 0 ~ "cat",
+            x == 1 ~ "dog",
+            x == 2 ~ "panda")
+}
+
+pred_test <- sapply(pred_test, decode) 
+
+head(pred_test, 10)
+
+confusionMatrix(as.factor(pred_test), 
+                as.factor(val_data$class))
--- 03_mushrooms_cnn.R
+++ 03_mushrooms_cnn.R
@@ -1,21 +1,15 @@
 # Tensorflow
 tensorflow::install_tensorflow()
-library(keras)
-library(tensorflow)
-
-# Use python in your anaconda3 environment folder
-reticulate::use_python("C:/Users/User/anaconda3/envs/tf_image", required = T)
-
-# Data wrangling
-library(dplyr)
-library(ggplot2)
-library(purrr)
-
-# Image manipulation
-library(imager)
 
 # Deep learning
 library(keras)
+library(tensorflow)
+
+# Data wrangling
+library(tidyverse)
+
+# Image manipulation
+library(imager)
 
 # Model Evaluation
 library(caret)
@@ -23,16 +17,19 @@ library(caret)
 
 # Prepare data set:
 # Data set downloaded from: https://www.kaggle.com/datasets/derekkunowilliams/mushrooms
-# 4 initial groups divided to 2 classes:
-# Edible & conditionally edible >> EDIBLE
-# Poisonous & deadly >> POISONOUS
+# 2 classes (from intial 4 classes):
+#   Edible: edible + conditionally edible
+#   Poisonous: poisonous + deadly
 
 # set directory paths
 train_dir <- "C:/Users/User/OneDrive/Edu/Deep Learning/mushroom_dataset/"
-train_edible_dir <- "C:/Users/User/OneDrive/Edu/Deep Learning/mushroom_dataset/edible/"
-train_poisonous_dir <- "C:/Users/User/OneDrive/Edu/Deep Learning/mushroom_dataset/poisonous/"
 
-folder_path <-c(train_edible_dir,train_poisonous_dir)
+folders <- list.files(train_dir)
+
+folders_paths <- paste0(train_dir,folders,'/') 
+
+train_edible_dir <- folders_paths[1]
+train_poisonous_dir <- folders_paths[2]
 
 # Desired height and width of images
 target_size <- c(128,128)
@@ -40,37 +37,36 @@ target_size <- c(128,128)
 # Batch size for training the model
 batch_size <- 32
 
-
-# Image Generator
+# Image generator
 train_data_gen <- image_data_generator(rescale = 1/255,
-                                      horizontal_flip = T,
-                                      vertical_flip = T,
-                                      fill_mode = 'nearest',
-                                      validation_split = 0.2)
+                                       horizontal_flip = T,
+                                       vertical_flip = T,
+                                       fill_mode = 'nearest',
+                                       validation_split = 0.2)
 
-# Training Dataset
+# Training dataset
 train_img_array <- flow_images_from_directory(directory = train_dir,
-                                        generator = train_data_gen,
-                                        classes = c("edible", "poisonous"),
-                                        target_size = target_size,
-                                        subset = 'training',
-                                        batch_size = batch_size,
-                                        color_mode = "rgb")
+                                              generator = train_data_gen,
+                                              classes = folders,
+                                              target_size = target_size,
+                                              subset = 'training',
+                                              batch_size = batch_size,
+                                              color_mode = "rgb")
 
-# Training Dataset
+# Training dataset
 valid_img_array <- flow_images_from_directory(directory = train_dir,
-                                        generator = train_data_gen,
-                                        classes = c("edible", "poisonous"),
-                                        target_size = target_size,
-                                        batch_size = batch_size,
-                                        subset = 'validation',
-                                        color_mode = "rgb")
+                                              generator = train_data_gen,
+                                              classes = folders,
+                                              target_size = target_size,
+                                              batch_size = batch_size,
+                                              subset = 'validation',
+                                              color_mode = "rgb")
 
 # Number of classes to predict:
 (output_n <- n_distinct(train_img_array$classes))
 
 # Define model architecture
-model <- keras_model_sequential() %>%
+simple_model <- keras_model_sequential() %>%
   
   layer_conv_2d(filters = 32,
                 kernel_size = c(3, 3),
@@ -79,7 +75,7 @@ model <- keras_model_sequential() %>%
   
   layer_batch_normalization() %>% 
   
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_average_pooling_2d(pool_size = c(2, 2)) %>%
   
   layer_flatten() %>%
   
@@ -88,62 +84,20 @@ model <- keras_model_sequential() %>%
   layer_dense(units = output_n, activation = "softmax")
 
 # Model summary
-model
+simple_model
 
 # Compile model
-model %>% compile(
-  loss = 'categorical_crossentropy',
-  optimizer = optimizer_adam(),
-  metrics ='accuracy')
-
-
-# Train model
-history <- model %>% fit_generator(train_generator,
-                                   steps_per_epoch = length(train_img_array),
-                                   epochs = 15,
-                                   validation_data = valid_img_array,
-                                   validation_steps = length(valid_img_array))
-
-
-# Define model architecture
-model <- keras_model_sequential() %>%
-  
-  layer_conv_2d(filters = 32,
-                kernel_size = c(3, 3),
-                activation = "relu",
-                input_shape = c(target_size, 3)) %>%
-  
-  layer_batch_normalization() %>% 
-  
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  
-  layer_conv_2d(filters = 64,
-                kernel_size = c(3, 3),
-                activation = "relu") %>%
-  
-  layer_batch_normalization() %>% 
-  
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
-  
-  layer_flatten() %>%
-  
-  layer_dense(units = 128, activation = "relu") %>%
-  
-  layer_dense(units = 64, activation = "relu") %>%
-  
-  layer_dense(units = output_n, activation = "softmax")
-
-# Compile model
-model %>% compile(
+simple_model %>% compile(
   loss = 'categorical_crossentropy',
   optimizer = optimizer_adam(),
   metrics ='accuracy')
 
 # Train model
-history <- model %>% fit_generator(train_img_array,
-                                   steps_per_epoch = length(train_img_array),
-                                   epochs = 15,
-                                   validation_data = valid_img_array,
-                                   validation_steps = length(valid_img_array))
+simple_model %>% fit_generator(train_img_array,
+                               steps_per_epoch = length(train_img_array),
+                               epochs = 15,
+                               validation_data = valid_img_array,
+                               validation_steps = length(valid_img_array)) -> simple_model
 
-plot(history)
+
+plot(simple_model)
